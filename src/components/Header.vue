a1ecfadbb9a5bff041169916843d3ba15c35a266
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-09 02:37:04 -0300

    emit searchPokemon added

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,22 @@
+<script setup lang="ts">
+    import { ref } from 'vue'
+
+    const emits = defineEmits(['fetchPokemons'])
+
+    const search = ref('')
+    const searchPokemon = async () =>{
+        emits('fetchPokemons', search.value)
+    }
+</script>
+
 <template>
-    <div class="navbar bg-[#eed9c4]">
+    <div class="navbar bg-[#c92a47]">
         <div class="navbar-start">
-            <div class="dropdown">
-                <div tabindex="0" role="button" class="btn btn-square btn-ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                </div>
-                <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><a>Homepage</a></li>
-                    <li><a>Portfolio</a></li>
-                    <li><a>About</a></li>
-                </ul>
-            </div>
         </div>
         <div class="navbar-end gap-2">
-            <div class="form-control">
-                <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
-            </div>
+            <form class="form-control" @submit.prevent="searchPokemon">
+                <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" v-model="search"/>
+            </form>
         </div>
     </div>
 </template>
